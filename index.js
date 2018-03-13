cd76f1bc4fecae0dbd00b7f383b41f57e4db0066
Andrew Joslin <andrewtjoslin@gmail.com>
2018-03-13 11:48:45 -0600

    Init

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 'use strict'
+
+module.exports = slsPromised
+
+slsPromised.response = opts => new SlsCustomResponse(opts)
+
+function slsPromised (handler) {
+  return slsPromisedWrapper
+
+  function slsPromisedWrapper (event, context, callback) {
+    return Promise.resolve()
+      .then(() => handler(event, context))
+      .then(function handleSuccess (result) {
+        if (!(result instanceof SlsCustomResponse)) {
+          result = new SlsCustomResponse({
+            body: result
+          })
+        }
+        callback(null, result)
+      }, function handleError (result) {
+        if (!(result instanceof Error)) {
+          result = Object.assign(new Error(), {
+            body: result || 'Interval Server Error'
+          })
+        }
+        result.statusCode = result.statusCode || 500
+        result.headers = result.headers || {
+          'Content-Type': 'application/json'
+        }
+        if (result.message && !result.body) {
+          result.body = result.message
+        }
+        callback(null, result)
+      })
+  }
+}
+
+function SlsCustomResponse (opts) {
+  this.statusCode = opts.statusCode || 200
+  this.headers = Object.assign({
+    'Content-Type': 'application/json'
+  }, opts.headers || {})
+  if (opts.body && this.headers['Content-Type'] === 'application/json' && typeof opts.body !== 'string') {
+    this.body = JSON.stringify(opts.body)
+  } else {
+    this.body = opts.body
+  }
+}
--- test.js
+++ test.js
@@ -1,8 +1,56 @@
 'use strict'
 
 var test = require('tape')
-var slsPromise = require('./')
+var slsPromised = require('./')
 
-test(function (t) {
+test('normal res', function (t) {
+  const event = {}
+  const context = {}
+  const fn = slsPromised((e, c) => {
+    t.equal(e, event)
+    t.equal(c, context)
+    return {is: 'ok'}
+  })
+  fn(event, context, (error, result) => {
+    t.notOk(error)
+    t.deepEqual(result, {
+      statusCode: 200,
+      body: JSON.stringify({is: 'ok'}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    t.end()
+  })
+})
 
+test('normal error', function (t) {
+  const fn = slsPromised(() => {
+    throw 'error' // eslint-disable-line
+  })
+  fn(null, null, (error, result) => {
+    t.notOk(error)
+    t.deepEqual(JSON.parse(JSON.stringify(result)), {
+      statusCode: 500,
+      body: 'error',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    t.end()
+  })
+})
+
+test('custom response', function (t) {
+  const customRes = slsPromised.response({
+    statusCode: 204,
+    body: null
+  })
+  const fn = slsPromised(() => customRes)
+
+  fn(null, null, (error, result) => {
+    t.notOk(error)
+    t.equal(result, customRes)
+    t.end()
+  })
 })
